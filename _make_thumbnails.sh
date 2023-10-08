02ce7fe0c002893a97d7febf3f8c1bf2b5341bc9
Ruixuan <ruixuanl@andrew.cmu.edu>
2023-10-08 01:20:15 -0400

    update

--- _make_thumbnails.sh
+++ _make_thumbnails.sh
@@ -1,3 +1,4 @@
 for file in images/*.{jpg,JPG,gif}; do
-    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 200x200 "tn/$file"
+    # [ ! -f "tn/$file" ] && convert "$file" -resize 400x "tn/$file"
+    [ ! -f "tn/$file" ] && convert "$file"  -thumbnail 400x "tn/$file"
 done
